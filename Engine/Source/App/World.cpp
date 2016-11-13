e211b2199bc3d605ec96397f63aa9dbf8854e247
dragn <dsabelnikov@gmail.com>
2016-11-13 20:06:49 +0300

    move_actor command

--- Engine/Source/App/World.cpp
+++ Engine/Source/App/World.cpp
@@ -61,6 +61,29 @@ DEFINE_COMMAND(pos_actor)
     actor->SetPos(V3(x, y, z));
 }
 
+DEFINE_COMMAND(move_actor)
+{
+    if (params.size() < 5)
+    {
+        LOG(INFO) << "Add position to actor";
+        LOG(INFO) << "Usage: " << params[0] << " <name> <X> <Y> <Z>";
+        return;
+    }
+
+    Actor* actor = App::Get<World>()->GetActorByName<Actor>(params[1]);
+    if (actor == nullptr)
+    {
+        LOG(INFO) << "Actor " << params[1] << " not found";
+        return;
+    }
+
+    float x = std::stof(params[2]);
+    float y = std::stof(params[3]);
+    float z = std::stof(params[4]);
+
+    actor->AddPos(V3(x, y, z));
+}
+
 const char* World::COMPONENT_NAME = "WORLD";
 
 World::World() : Component(COMPONENT_NAME, 16)
@@ -73,6 +96,7 @@ void World::TickInit(uint32_t delta)
     REGISTER_COMMAND(destroy_actor);
     REGISTER_COMMAND(spawn_actor);
     REGISTER_COMMAND(pos_actor);
+    REGISTER_COMMAND(move_actor);
 
     FinishInit();
 }
